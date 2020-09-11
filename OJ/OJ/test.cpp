fd8609417b017d43244278e123b48b75772d39e4
Coderxingchild <1272209351@qq.com>
2020-09-11 22:18:00 +0800

    OJ

--- OJ/OJ/test.cpp
+++ OJ/OJ/test.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<windows.h>
+#include<assert.h>
+#include<iostream>
+using namespace std;
 
 
 //1.将两个升序链表合并成为一个升序链表
@@ -89,3 +92,105 @@
 
 
 
+//  3、8 bit 位；在指定位置数字置 0 或 1 ，true 置 1；flase 置 0
+
+
+//void bit_set(unsigned char* p_data, unsigned char position, bool flag)
+//{
+//	//方法二
+//	if (flag) {
+//		*p_data |= (0x01 << (position - 1));
+//	}
+//	else {
+//		*p_data &= ~(0x01 << (position - 1));
+//	}
+//
+//
+//	//方法一
+//	//if (flag)
+//	//{
+//	//	switch (position){   //真 ，置 1
+//	//	case 1:
+//	//		*p_data |= 0x00;  //0000 0001
+//	//		break;
+//	//	case 2:
+//	//		*p_data |= 0x02;  //0000 0010
+//	//		break;
+//	//	case 3:
+//	//		*p_data |= 0x04;  //0000 0100
+//	//		break;
+//	//	case 4:
+//	//		*p_data |= 0x08;  //0000 1000
+//	//		break;
+//	//	case 5:
+//	//		*p_data |= 0x10;  //0001 0000
+//	//		break;
+//	//	case 6:
+//	//		*p_data |= 0x20;  //0010 0000
+//	//	case 7:
+//	//		*p_data |= 0x40; //0100 0000
+//	//		break;
+//	//	case 8:
+//	//		*p_data |= 0x80;  //1000 0000
+//	//	}
+//	//}
+//	//else
+//	//{
+//	//	switch (position) {
+//	//	case 1:
+//	//		*p_data &= ~0x00;  //1111 11110
+//	//		break;
+//	//	case 2:
+//	//		*p_data &= ~0x02;  //1111 1101
+//	//		break;
+//	//	case 3:
+//	//		*p_data &= ~0x04;  //1111 1011
+//	//		break;
+//	//	case 4:
+//	//		*p_data &= ~0x08;  //1111 0111
+//	//		break;
+//	//	case 5:
+//	//		*p_data &= ~0x10;  //1110 1111
+//	//		break;
+//	//	case 6:
+//	//		*p_data &= ~0x20;  //1101 1111
+//	//	case 7:
+//	//		*p_data &= ~0x40; //1011 1111
+//	//		break;
+//	//	case 8:
+//	//		*p_data &= ~0x80;  //0111 1111
+//	//	}
+//	//}
+//}
+//void main()
+//{
+//	unsigned char data = 123;
+//	bit_set(&data, 2, false);
+//	cout << (int)data << endl;
+//}
+
+
+// 4、实现字符串的右移
+
+//void RightLoopMove(char *pStr, unsigned short steps)
+//{
+//	assert(pStr != NULL);
+//	int len = strlen(pStr);
+//	char *tmp = (char*)malloc(sizeof(char)*(len+1));
+//	assert(tmp != NULL);
+//	memset(tmp, 0, sizeof(char)*(len + 1));
+//	steps %= len;
+//
+//	strcpy(tmp, pStr + (len - steps));
+//	strncat(tmp, pStr, len - steps);
+//	strcpy(pStr, tmp);
+//	free(tmp);
+//	tmp = NULL;
+//}
+//void main()
+//{
+//	char str[] = "abcdefghi"; 
+//	cout << str << endl;
+//	RightLoopMove(str, 2);
+//	cout << str << endl;
+//}
